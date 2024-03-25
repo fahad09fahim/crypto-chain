3ca91fada1e51189113ee40ae70dac933233c490
Fahim <anik.fahim07@gmail.com>
2024-03-25 23:36:28 +0600

    completed statistics page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./component/header/Header"
 import Banner from './component/sections/Banner';
 import Investor from "./component/sections/investor/Investor";
 import Process from "./component/sections/process/Process";
+import Statistics from "./component/statistics/Statistics";
 
 
 
@@ -16,6 +17,7 @@ function App() {
       <Banner/>
       <Investor/>
       <Process/>
+      <Statistics/>
     </>
   )
 }
---
+++ src/component/statistics/Statistics.jsx
@@ -0,0 +1,24 @@
+
+
+const Statistics = () => {
+    return (
+        <div className="flex justify-center">
+            <div className="grid grid-cols-1 md:grid-cols-3 text-center gap-6">
+                  <div>
+                   <h2 className="text-2xl md:text-4xl font-semibold text-colorOne ">3000+</h2>
+                   <p className="text-custom-black">Wallet Connect</p>
+                  </div>
+                  <div>
+                   <h2 className="text-2xl md:text-4xl font-semibold text-colorOne ">50,000+</h2>
+                   <p className="text-custom-black">Collections Indexed every 5 mins.</p>
+                  </div>
+                  <div>
+                  <h2 className="text-2xl md:text-4xl font-semibold text-colorOne ">4.7X</h2>
+                  <p className="text-custom-black">Difference in Floor & Estimated Value</p>
+                  </div>
+            </div>
+        </div>
+    );
+};
+
+export default Statistics;
